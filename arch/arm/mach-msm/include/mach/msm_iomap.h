0a22b4bb56d3b8b2806847487fdb725ee12cf904
Dima Zavin <dima@android.com>
2009-06-30 17:16:24 -0700

    [ARM] msm: qsd8k: Add GPU memory range to iomap
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/mach-msm/include/mach/msm_iomap.h
+++ arch/arm/mach-msm/include/mach/msm_iomap.h
@@ -157,6 +157,11 @@
 #define MSM_SCPLL_SIZE        SZ_4K 
 #endif
 
+#if defined(CONFIG_ARCH_QSD8X50)
+#define MSM_GPU_REG_PHYS      0xA0000000
+#define MSM_GPU_REG_SIZE      0x00020000
+#endif
+
 
 #ifdef CONFIG_MSM_DEBUG_UART
 #define MSM_DEBUG_UART_BASE   0xE1000000
