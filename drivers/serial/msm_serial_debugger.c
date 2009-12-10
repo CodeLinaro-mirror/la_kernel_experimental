c30717f22969e506112e9627c20d3a5718953c98
Arve Hjønnevåg <arve@android.com>
2009-12-09 23:26:18 -0800

    msm_serial_debugger build on 2.6.32
    Change-Id: I05b34d35181fbbe5d3b204a6a7a43a634d539654

--- drivers/serial/msm_serial_debugger.c
+++ drivers/serial/msm_serial_debugger.c
@@ -202,14 +202,14 @@ static void dump_irqs(void)
 	dprintf("irqnr       total  since-last   status  name\n");
 	for (n = 1; n < NR_IRQS; n++) {
 		struct irqaction *act = irq_desc[n].action;
-		if (!act && !kstat_cpu(0).irqs[n])
+		if (!act && !kstat_irqs(n))
 			continue;
 		dprintf("%5d: %10u %11u %8x  %s\n", n,
-			kstat_cpu(0).irqs[n],
-			kstat_cpu(0).irqs[n] - last_irqs[n],
+			kstat_irqs(n),
+			kstat_irqs(n) - last_irqs[n],
 			irq_desc[n].status,
 			(act && act->name) ? act->name : "???");
-		last_irqs[n] = kstat_cpu(0).irqs[n];
+		last_irqs[n] = kstat_irqs(n);
 	}
 }
 
