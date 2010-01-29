62f9cdc3c4f990a91350d12f6f49d80bd758c20c
Dima Zavin <dima@android.com>
2010-01-29 11:43:42 -0800

    arm: msm: correctly signal the apps-to-modem irq in smd/proc_comm
    Change-Id: I85d8b1ff65cea9197df17dc5131061bbac0a08e9
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/mach-msm/io.c
+++ arch/arm/mach-msm/io.c
@@ -57,6 +57,9 @@ static struct map_desc msm_io_desc[] __initdata = {
 #ifdef CONFIG_ARCH_MSM_SCORPION
 	MSM_DEVICE(TCSR),
 #endif
+#ifdef CONFIG_ARCH_MSM7X30
+	MSM_DEVICE(GCC),
+#endif
 	{
 		.virtual =  (unsigned long) MSM_SHARED_RAM_BASE,
 		.pfn =      __phys_to_pfn(MSM_SHARED_RAM_PHYS),
