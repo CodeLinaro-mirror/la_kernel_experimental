46368c451d6cbe2410658d7a75aa9685d607294a
Colin Cross <ccross@android.com>
2010-02-10 17:13:07 -0800

    [ARM] tegra: harmony: Add harmony board file
    Signed-off-by: Colin Cross <ccross@android.com>

---
+++ arch/arm/mach-tegra/board-harmony.c
@@ -0,0 +1,27 @@
+/*
+ *
+ * Copyright (C) 2010 Google, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <linux/kernel.h>
+
+#include <asm/mach-types.h>
+#include <asm/mach/arch.h>
+
+#include <mach/board.h>
+
+MACHINE_START(HARMONY, "harmony")
+
+    .boot_params  = 0x00000100,
+
+MACHINE_END
