bb460adaf05676cd904301c230a370dd6af74989
Dima Zavin <dima@android.com>
2011-01-05 11:53:36 -0800

    yaffs: Import yaffs from Thu Dec 23 13:31:37 2010 +1300
    commit ddf33fed15c2376bfb602d62dd018c63fce60df8
    Author: Timothy Manning <tfhmanning@gmail.com>
    Date:   Thu Dec 23 13:31:37 2010 +1300
    
        yaffs updated direct/timothy_tests/quick_tests
        Signed-off-by: Timothy Manning <tfhmanning@gmail.com>
    
    Change-Id: I5bbe5a05277bdf8a6fe188bbe4c77725b3fa2aae
    Signed-off-by: Dima Zavin <dima@android.com>

--- fs/yaffs2/yaffs_attribs.h
+++ fs/yaffs2/yaffs_attribs.h
@@ -13,9 +13,16 @@
  * Note: Only YAFFS headers are LGPL, YAFFS C code is covered by GPL.
  */
 
-#ifndef __YAFFSINTERFACE_H__
-#define __YAFFSINTERFACE_H__
+#ifndef __YAFFS_ATTRIBS_H__
+#define __YAFFS_ATTRIBS_H__
 
-int yaffs_Initialise(unsigned nBlocks);
+#include "yaffs_guts.h"
+
+void yaffs_load_attribs(struct yaffs_obj *obj, struct yaffs_obj_hdr *oh);
+void yaffs_load_attribs_oh(struct yaffs_obj_hdr *oh, struct yaffs_obj *obj);
+void yaffs_attribs_init(struct yaffs_obj *obj, u32 gid, u32 uid, u32 rdev);
+void yaffs_load_current_time(struct yaffs_obj *obj, int do_a, int do_c);
+int yaffs_set_attribs(struct yaffs_obj *obj, struct iattr *attr);
+int yaffs_get_attribs(struct yaffs_obj *obj, struct iattr *attr);
 
 #endif
--- fs/yaffs2/yaffs_yaffs1.h
+++ fs/yaffs2/yaffs_yaffs1.h
@@ -13,21 +13,10 @@
  * Note: Only YAFFS headers are LGPL, YAFFS C code is covered by GPL.
  */
 
-#ifndef __YAFFS_CHECKPTRW_H__
-#define __YAFFS_CHECKPTRW_H__
+#ifndef __YAFFS_YAFFS1_H__
+#define __YAFFS_YAFFS1_H__
 
 #include "yaffs_guts.h"
-
-int yaffs2_checkpt_open(struct yaffs_dev *dev, int writing);
-
-int yaffs2_checkpt_wr(struct yaffs_dev *dev, const void *data, int n_bytes);
-
-int yaffs2_checkpt_rd(struct yaffs_dev *dev, void *data, int n_bytes);
-
-int yaffs2_get_checkpt_sum(struct yaffs_dev *dev, u32 * sum);
-
-int yaffs_checkpt_close(struct yaffs_dev *dev);
-
-int yaffs2_checkpt_invalidate_stream(struct yaffs_dev *dev);
+int yaffs1_scan(struct yaffs_dev *dev);
 
 #endif
