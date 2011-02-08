01d67885008ea089a84cc36d5cf02e1913bc6c34
Liam Girdwood <lrg@slimlogic.co.uk>
2011-02-08 11:45:20 +0000

    ASoC: dapm - allow custom widgets to update power events.
    Allow for the operation of custom mixer and mux DAPM widgets that
    can call snd_soc_dapm_mixer_update_power() and
    snd_soc_dapm_mux_update_power() directly after updating their
    status.
    
    This adds two previously static calls to the public DAPM API.
    
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -50,6 +50,8 @@
 
 #define PATH_MAX_HOPS 16
 
+int soc_dsp_runtime_update(struct snd_soc_dapm_widget *);
+
 /* dapm power sequences - make this per codec in the future */
 static int dapm_up_seq[] = {
 	[snd_soc_dapm_pre] = 0,
@@ -1563,7 +1565,7 @@ void snd_soc_dapm_debugfs_init(struct snd_soc_dapm_context *dapm)
 #endif
 
 /* test and update the power status of a mux widget */
-static int dapm_mux_update_power(struct snd_soc_dapm_widget *widget,
+int snd_soc_dapm_mux_update_power(struct snd_soc_dapm_widget *widget,
 				 struct snd_kcontrol *kcontrol, int change,
 				 int mux, struct soc_enum *e)
 {
@@ -1579,7 +1581,7 @@ static int dapm_mux_update_power(struct snd_soc_dapm_widget *widget,
 		return 0;
 
 	/* find dapm widget path assoc with kcontrol */
-	list_for_each_entry(path, &widget->dapm->card->paths, list) {
+	list_for_each_entry(path, &widget->dapm->paths, list) {
 		if (path->kcontrol != kcontrol)
 			continue;
 
@@ -1594,15 +1596,18 @@ static int dapm_mux_update_power(struct snd_soc_dapm_widget *widget,
 			path->connect = 0; /* old connection must be powered down */
 	}
 
-	if (found)
+	if (found) {
 		dapm_power_widgets(widget->dapm, SND_SOC_DAPM_STREAM_NOP);
+		soc_dsp_runtime_update(widget);
+	}
 
 	return 0;
 }
+EXPORT_SYMBOL_GPL(snd_soc_dapm_mux_update_power);
 
 /* test and update the power status of a mixer or switch widget */
-static int dapm_mixer_update_power(struct snd_soc_dapm_widget *widget,
-				   struct snd_kcontrol *kcontrol, int connect)
+int snd_soc_dapm_mixer_update_power(struct snd_soc_dapm_widget *widget,
+		struct snd_kcontrol *kcontrol, int connect)
 {
 	struct snd_soc_dapm_path *path;
 	int found = 0;
@@ -1623,11 +1628,14 @@ static int dapm_mixer_update_power(struct snd_soc_dapm_widget *widget,
 		break;
 	}
 
-	if (found)
+	if (found) {
 		dapm_power_widgets(widget->dapm, SND_SOC_DAPM_STREAM_NOP);
+		soc_dsp_runtime_update(widget);
+	}
 
 	return 0;
 }
+EXPORT_SYMBOL_GPL(snd_soc_dapm_mixer_update_power);
 
 /* show dapm widget status in sys fs */
 static ssize_t dapm_widget_show(struct device *dev,
@@ -2129,7 +2137,7 @@ int snd_soc_dapm_put_volsw(struct snd_kcontrol *kcontrol,
 		update.val = val;
 		widget->dapm->update = &update;
 
-		dapm_mixer_update_power(widget, kcontrol, connect);
+		snd_soc_dapm_mixer_update_power(widget, kcontrol, connect);
 
 		widget->dapm->update = NULL;
 	}
@@ -2210,7 +2218,7 @@ int snd_soc_dapm_put_enum_double(struct snd_kcontrol *kcontrol,
 	update.val = val;
 	widget->dapm->update = &update;
 
-	dapm_mux_update_power(widget, kcontrol, change, mux, e);
+	snd_soc_dapm_mux_update_power(widget, kcontrol, change, mux, e);
 
 	widget->dapm->update = NULL;
 
@@ -2260,7 +2268,7 @@ int snd_soc_dapm_put_enum_virt(struct snd_kcontrol *kcontrol,
 
 	change = widget->value != ucontrol->value.enumerated.item[0];
 	widget->value = ucontrol->value.enumerated.item[0];
-	dapm_mux_update_power(widget, kcontrol, change, widget->value, e);
+	snd_soc_dapm_mux_update_power(widget, kcontrol, change, widget->value, e);
 
 	mutex_unlock(&widget->codec->mutex);
 	return ret;
@@ -2352,7 +2360,7 @@ int snd_soc_dapm_put_value_enum_double(struct snd_kcontrol *kcontrol,
 	update.val = val;
 	widget->dapm->update = &update;
 
-	dapm_mux_update_power(widget, kcontrol, change, mux, e);
+	snd_soc_dapm_mux_update_power(widget, kcontrol, change, mux, e);
 
 	widget->dapm->update = NULL;
 
